267dd2d6af4684a74d09aec07c320a9caa641b6f
Seb <78613780+sub2900@users.noreply.github.com>
2023-05-25 13:27:32 -0400

    Update consts.js

--- constants/consts.js
+++ constants/consts.js
@@ -1,1 +1,13 @@
+// The bitwise operator in js is used similar way as in V
+const constants = {
+    banned: 1 << 0,
+    user: 1 <<1,
+    verified: 1 << 2,
+    supporter: 1 << 3,
+    bat: 1 << 4,
+    moderator: 1 << 5,
+    admin: 1 << 6,
+    dev: 1 << 7,
+    pending: 1 << 8
 
+}
